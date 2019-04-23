3fac7d3c9b7b43ec7e1a89cb4ce2bd6711d951c4
Lon Hohberger <lon@metamorphism.com>
2019-04-23 14:00:19 -0400

    Add actual delta function

--- tag_utils/delta.py
+++ tag_utils/delta.py
@@ -4,6 +4,8 @@ import koji_wrapper
 from koji_wrapper.base import KojiWrapperBase
 from koji_wrapper.tag import KojiTag
 
+from toolchest.rpm.utils import labelCompare
+from toolchest.rpm.utils import splitFilename
 
 from tag_utils.common import latest_tagged_as_nevr
 from tag_utils.compose import compose_as_nevr
@@ -51,3 +53,60 @@ def input_to_nevr_dict(inp):
         return tag_to_latest_builds(inp)
 
     raise ValueError('Unhandled type: ' + str(type(inp)))
+
+
+# Returns:
+#   - removed: components+builds which are in left, but
+#              not in right
+#   - added: components (and corresponding builds) which are
+#            in right, but not in left
+#   - common: components (and builds) which are equal between
+#             left and right
+#   - downgrades: components/builds where the n-e:v-r has decreased
+#                 from left to right
+#   - upgrades: components/builds where the n-e:v-r has increased
+#               from left to right
+def delta(inp_left, inp_right):
+    left = input_to_nevr_dict(inp_left)
+    right = input_to_nevr_dict(inp_right)
+
+    lc = set(left.keys())
+    rc = set(right.keys())
+
+    removed_comps = sorted(list(lc - rc))
+    removed = {}
+    for comp in removed_comps:
+        removed[comp] = left[comp]
+
+    added_comps = sorted(list(rc - lc))
+    added = {}
+    for comp in added_comps:
+        added[comp] = right[comp]
+
+    common_comps = sorted(list(lc & rc))
+    common = {}
+    downgrades = {}
+    upgrades = {}
+    # Break this one up in a more granular fashion
+    for comp in common_comps:
+        lnevr = left[comp]
+        rnevr = right[comp]
+
+        (ln, lv, lr, le, la) = splitFilename(lnevr)
+        (rn, rv, rr, re, ra) = splitFilename(rnevr)
+        ret = labelCompare((le, lv, lr), (re, rv, rr))
+        if ret > 0:
+            downgrades[comp] = [lnevr, rnevr]
+        elif ret < 0:
+            upgrades[comp] = [lnevr, rnevr]
+        else:
+            common[comp] = lnevr
+
+    ret = {}
+    ret['removed'] = removed
+    ret['added'] = added
+    ret['common'] = common
+    ret['downgrades'] = downgrades
+    ret['upgrades'] = upgrades
+
+    return ret
