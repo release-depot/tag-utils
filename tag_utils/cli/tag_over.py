e3c9db4a73752fc6186b06a3e703830047bbd262
Lon Hohberger <lhh@redhat.com>
2019-12-03 09:42:57 -0500

    New Feature: Separate destination tag
    It's useful to compare tags A and B, but then tag things to
    tag C instead of B.  You use this if, for example, you want
    to compare what was shipped to what is queued up in a "trunk"
    tag, but then tag all the updates into the proper "candidate"
    tag.
    
    Signed-off-by: Lon Hohberger <lhh@redhat.com>

--- tag_utils/cli/tag_over.py
+++ tag_utils/cli/tag_over.py
@@ -17,10 +17,30 @@ def chunky(stuff, length):
 def main():
     import sys
     argv = sys.argv
+    missing = False
+    display = False
+    dest_tag = None
 
     if len(argv) < 3:
-        print(f'usage: {argv[0]} tag_or_compose target_tag')
+        print(f'usage: {argv[0]} [--missing|--dest desttag] tag_or_compose tag')
+        print()
+        print('Compare \"tag_or_compose\" with \"tag\" and tag anything that would be')
+        print('an upgrade to \"tag\".  If \"desttag\" is specified, tag packages')
+        print('to \"desttag\" instead of \"tag\".')
         return 1
+
+    # XXX switch to argparser if we get any more
+    while argv[1].startswith('--'):
+        arg = argv.pop(1)
+        if arg == '--print':
+            display = True
+        if arg == '--missing':
+            missing = True
+        if arg == '--dest':
+            dest_tag = argv.pop(1)
+
+    if not dest_tag:
+        dest_tag = argv[2]
 
     delta_info = delta(argv[1], argv[2])
 
@@ -28,8 +48,17 @@ def main():
     builds = []
     for comp in delta_info['downgrades']:
         builds.append(drop_epoch(delta_info['downgrades'][comp]['old']))
+    if missing:
+        for comp in delta_info['removed']:
+            builds.append(drop_epoch(delta_info['removed'][comp]))
 
-    tag_over = ['brew', 'tag-pkg', argv[2]]
+    # Simply print what we'd tag
+    if display:
+        for b in builds:
+            print(b)
+        return
+
+    tag_over = ['brew', 'tag-pkg', dest_tag]
     chunked_builds = chunky(builds, 10)
     for b in chunked_builds:
         cmd = copy.copy(tag_over)
